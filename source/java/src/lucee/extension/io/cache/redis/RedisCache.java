0f7d5c5478f32aa55b33d09d96643fad207bc961
David Rogers <david@inleague.io>
2023-11-30 16:23:06 -0600

    add failing test "does not return 'the same object' from a cacheGet call"
    adds hook to inject thread delays for testing purposes

--- source/java/src/lucee/extension/io/cache/redis/RedisCache.java
+++ source/java/src/lucee/extension/io/cache/redis/RedisCache.java
@@ -77,6 +77,14 @@ public class RedisCache extends CacheSupport implements Command {
 
 	private final Object token = new Object();
 
+	/**
+	 * Boxed type is to support representing the null case
+	 */
+	private Integer __test__writeCommitDelay_ms = null;
+	public Integer get__test__writeCommitDelay_ms() {
+		return __test__writeCommitDelay_ms;
+	}
+
 	public RedisCache() {
 		if (async) {
 			// storage = new Storage(this);
@@ -100,6 +108,9 @@ public class RedisCache extends CacheSupport implements Command {
 	public void init(Config config, Struct arguments) throws IOException {
 		this.cl = arguments.getClass().getClassLoader();
 		if (config == null) config = CFMLEngineFactory.getInstance().getThreadConfig();
+
+		__test__writeCommitDelay_ms = caster.toIntValue(arguments.get("__test__writeCommitDelay_ms", null), 0);
+		__test__writeCommitDelay_ms = __test__writeCommitDelay_ms <= 0 ? null : __test__writeCommitDelay_ms;
 
 		host = caster.toString(arguments.get("host", "localhost"), "localhost");
 		port = caster.toIntValue(arguments.get("port", null), 6379);
@@ -959,6 +970,10 @@ public class RedisCache extends CacheSupport implements Command {
 					}
 					synchronized (tokenAddToNear) {
 						if (entries.isEmpty()) tokenAddToNear.wait();
+					}
+
+					if (cache.get__test__writeCommitDelay_ms() != null) {
+						Thread.sleep(cache.get__test__writeCommitDelay_ms());
 					}
 				}
 				catch (Throwable e) {
